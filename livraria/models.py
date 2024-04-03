994a1684068772b153c7aa1b7e05acde61f3144a
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-04-03 11:25:18 -0300

    Formatação dos arquivos do projeto

--- livraria/models.py
+++ livraria/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Categoria(models.Model):
-    descricao= models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
@@ -12,4 +14,3 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
-    
